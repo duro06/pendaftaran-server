f1c533681c87c00246269fbd06f86723349d5717
duro06 <andi.meka.025@gmail.com>
2021-07-24 22:11:33 +0700

    type nilai in action

--- app/Http/Controllers/API/AdminController.php
+++ app/Http/Controllers/API/AdminController.php
@@ -10,12 +10,114 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 
-class MeController extends Controller{
+class AdminController extends Controller{
 
-    public function add_mapel(Request $req){}
-    public function edit_mapel(Request $req){}
-    public function delete_mapel(Request $req){}
-    public function add_type(Request $req){}
-    public function edit_type(Request $req){}
-    public function delete_type(Request $req){}
+    public function add_mapel(Request $req){
+        $req->validate([
+                'name'=>'required',                
+                'type_id'=>'required',                
+            ]);
+            
+            $mapel=Mapel::create([
+                'name'=>$req->name,
+                'type_id'=>$req->type_id,
+            ]);
+        if($mapel){
+            return response()->json([
+                'mapel'=>$mapel,                
+            ],200);
+        }else{
+            
+            return response()->json([
+                'message'=>'failed',                
+            ],500);
+        }
+    }
+    public function edit_mapel(Request $req){
+        $req->validate([
+                'name'=>'required',                
+                'id'=>'required',                
+            ]);
+        $mapel=Mapel::find($req->id);
+        $mapel->name=$req->name;
+        if($mapel->save()){
+            return response()->json([
+                'mapel'=>$mapel,                
+            ],200);
+        }else{
+            
+            return response()->json([
+                'message'=>'failed',                
+            ],500);
+        }
+    }
+    public function delete_mapel(Request $req){
+         $req->validate([
+                'id'=>'required',                
+            ]);
+            $mapel=Mapel::find($req->id);
+        if($mapel->delete()){
+            return response()->json([
+                'mapel'=>$mapel,                
+            ],200);
+        }else{
+            
+            return response()->json([
+                'message'=>'failed',                
+            ],500);
+        }
+    }
+    public function add_type(Request $req){
+        $req->validate([
+                'name'=>'required',                
+            ]);
+            
+            $type=Type::create([
+                'name'=>$req->name,
+            ]);
+        if($type){
+            return response()->json([
+                'type'=>$type,                
+            ],200);
+        }else{
+            
+            return response()->json([
+                'message'=>'failed',                
+            ],500);
+        }
+    }
+    public function edit_type(Request $req){
+        $req->validate([
+                'name'=>'required',                
+                'id'=>'required',                
+            ]);
+        $type=Type::find($req->id);
+        $type->name=$req->name;
+        if($type->save()){
+            return response()->json([
+                'type'=>$type,                
+            ],200);
+        }else{
+            
+            return response()->json([
+                'message'=>'failed',                
+            ],500);
+        }
+    }
+    public function delete_type(Request $req){
+        $req->validate([
+                'id'=>'required',                
+            ]);
+            $type=Type::find($req->id);
+        if($type->delete()){
+            return response()->json([
+                'type'=>$type,                
+            ],200);
+        }else{
+            
+            return response()->json([
+                'message'=>'failed',                
+            ],500);
+        }
+    }
 }
